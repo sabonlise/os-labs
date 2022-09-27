81b3edf56748596fb5337b3cdfd4488a1ed86660
sabonlise <46754745+sabonlise@users.noreply.github.com>
2022-09-27 21:59:01 +0300

    Update ex4.c

--- week4/ex4.c
+++ week4/ex4.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char command[153];
     scanf("%[^\n]s", command);
-
+    
+    if (command[strlen(command) - 1] != '&') strcat(command, " &");
+    
     system(command);
 
     return EXIT_SUCCESS;
