dbf11e2f0ab0989223aa9c558ef14576193434ec
sabonlise <46754745+sabonlise@users.noreply.github.com>
2022-10-02 23:21:44 +0300

    Update subscriber.c

--- week5/subscriber.c
+++ week5/subscriber.c
@@ -4,23 +4,31 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
 #define MAX_SIZE 1024
 
 
-int main() {
+int main(int argc, char *argv[]) {
     int fd;
     char received_message[MAX_SIZE], *ipc = "/tmp/ex1";
 
-    fd = open(ipc, O_RDONLY | O_NONBLOCK);
+    int subscribers = atoi(argv[1]);
+    if (subscribers == 0) {
+        subscribers = 1;
+    }
+
+    fd = open(ipc, O_RDONLY);
 
     while(received_message) {
         if((read(fd, received_message, MAX_SIZE)) <= 0) break;
-        printf("Received message: %s\n", received_message);
+        
+        for (int i = 0; i < subscribers; i++) {
+            printf("Received message: %s\n", received_message);
+            sleep(1);
+        }
     }
 
     close(fd);
 
     return EXIT_SUCCESS;
 }
-
-  
