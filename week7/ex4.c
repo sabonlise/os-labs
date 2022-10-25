7803333ca7a43a252d5b04483f92e2eb6467c101
sabonlise <46754745+sabonlise@users.noreply.github.com>
2022-10-25 19:42:39 +0300

    Update ex4.c

--- week7/ex4.c
+++ week7/ex4.c
@@ -3,18 +3,22 @@
 
 
 void* __realloc(void *ptr, int new_size) {
+    void *new_ptr;
+
     if (new_size == 0) {
         free(ptr);
         return NULL;
     }
 
-    void *new_ptr = malloc(new_size * sizeof(void *));
-    size_t old_size = malloc_usable_size(ptr);
-
-    if (old_size == 0) return new_ptr;
-
-    size_t copy_size = old_size < new_size ? old_size : new_size;
-    memcpy(new_ptr, ptr, copy_size);
+    if (ptr == NULL) {
+        new_ptr = malloc(new_size);
+    } else  {
+        new_ptr = malloc(new_size * sizeof(ptr));
+        size_t old_size = malloc_usable_size(ptr);
+        
+        size_t copy_size = old_size < new_size ? old_size : new_size;
+        memcpy(new_ptr, ptr, copy_size);
+    }
 
     return new_ptr;
 }
