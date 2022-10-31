803002eb34358b8888a854a5066705a2ea12b177
sabonlise <46754745+sabonlise@users.noreply.github.com>
2022-10-31 21:18:29 +0300

    Update ex2.c

--- week8/ex2.c
+++ week8/ex2.c
@@ -9,6 +9,8 @@ int main() {
     size_t size = 1024 * 1024 * 10;
     while (seconds++ != 5) {
         int *p = malloc(size);
+        // si and so fields are all zeros because (probably) 10 MB 
+        // is not enough to be loaded on disk
         memset(p, 0, size);
         printf("Allocating 10 MB of space\n");
         sleep(1);
