d1f7155be13ce3e5bfdfb984b77394b1267f276b
sabonlise <46754745+sabonlise@users.noreply.github.com>
2022-10-31 21:36:07 +0300

    Create ex4.c

---
+++ week8/ex4.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/resource.h>
+
+
+int main() {
+    struct rusage r_usage;
+
+    int seconds = 0;
+    size_t size = 1024 * 1024 * 10;
+    while (seconds++ != 5) {
+        int *p = malloc(size);
+        memset(p, 0, size);
+        getrusage(RUSAGE_SELF, &r_usage);
+        printf("Memory usage: %ld Mb\n", r_usage.ru_maxrss / 1024);
+        sleep(1);
+    }
+
+    return EXIT_SUCCESS;
+}
